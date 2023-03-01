722dccdc83ca274de8105f29a2cff1bdc48a77a0
523jadams <92735748+523jadams@users.noreply.github.com>
2023-03-01 13:19:05 -0500

    Milestone 1
    finished steps 1-4

--- site.js
+++ site.js
@@ -22,24 +22,29 @@
 
 
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.movies = json;
+      });
+  },
+  data() {
+    return {
+      // This holds your movies.json data.
+      movies: [],
+      /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+      //added the values for my variables
+      title: "IMDB + Jake's Top 8 Movies",
+      owner: "Jake",
+      github: "#",
+    };
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+  },
+});
 
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
